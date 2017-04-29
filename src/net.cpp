82d22f85e3ee3932e0ef232318b8b181ee934e52
arthearts <hnaoume@gmail.com>
2017-04-29 14:57:26 -0300

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1060,8 +1060,7 @@ void ThreadMapPort()
 #ifndef UPNPDISCOVER_SUCCESS
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-#else
- #elif MINIUPNPC_API_VERSION < 14
+#elif MINIUPNPC_API_VERSION < 14
     /* miniupnpc 1.6 */
     int error = 0;
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
